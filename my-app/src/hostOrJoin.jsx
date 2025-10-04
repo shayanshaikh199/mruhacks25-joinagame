c7965a11e51b991903f3f42f3170530ac13f4acd
Shayan Shaikh <shayan.shaikh@ucalgary.ca>
2025-10-04 12:30:54 -0600

    recommit

--- my-app/src/hostOrJoin.jsx
+++ my-app/src/hostOrJoin.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+//test
 export default function HostOrJoin({ selectedSport }) {
     // use react router to move between pages
     const navigate = useNavigate();
