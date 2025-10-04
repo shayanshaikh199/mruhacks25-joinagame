fc021001ceed0b1403bc584135d30d3fe67258af
Shayan Shaikh <shayan.shaikh@ucalgary.ca>
2025-10-04 12:01:12 -0600

    react roouter to switch between pages

--- my-app/src/hostOrJoin.jsx
+++ my-app/src/hostOrJoin.jsx
@@ -1,2 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+
+export default function HostOrJoin({ selectedSport }) {
+    // use react router to move between pages
+    const navigate = useNavigate();
+}
